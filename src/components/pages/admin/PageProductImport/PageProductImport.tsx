dd96eb4f9655a51a8427e734f191b0a9360b5917
Max Korsakov <Maksim_Karsakou1epam.com>
2020-12-03 10:19:53 +0300

    add cart

--- src/components/pages/admin/PageProductImport/PageProductImport.tsx
+++ src/components/pages/admin/PageProductImport/PageProductImport.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import API_PATHS from "constants/apiPaths";
 import ProductsTable from "components/pages/admin/PageProductImport/components/ProductsTable";
@@ -14,8 +14,14 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const TOKON_FOR_AUTH = 'TWF4X0tvcnNha292OlRFU1RfUEFTU1dPUkQ=';
+
 export default function PageProductImport() {
   const classes = useStyles();
+
+  useEffect(()=>{
+    localStorage.setItem('auth', TOKON_FOR_AUTH);
+  },[])
 
   return (
     <div className={classes.content}>
--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://320ihl0yyc.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://320ihl0yyc.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://320ihl0yyc.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://k68skd3820.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://320ihl0yyc.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
